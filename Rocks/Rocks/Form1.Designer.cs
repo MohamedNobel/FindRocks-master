00b316edb869f58985233fc6cdbec0bff7738e8c
MohamedNobel <mnabil199327@gmail.com>
2017-04-05 17:21:40 +0200

    Rocks
    Find And Count For String by Numbers
    Rocks and Elements

---
+++ Rocks/Rocks/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Rocks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumberTxt = new System.Windows.Forms.TextBox();
+            this.LetterTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ATxt = new System.Windows.Forms.TextBox();
+            this.ABTxt = new System.Windows.Forms.TextBox();
+            this.BTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LetterLTxt = new System.Windows.Forms.TextBox();
+            this.ATtxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BTtxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ABTtext = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label1.ForeColor = System.Drawing.Color.MistyRose;
+            this.label1.Location = new System.Drawing.Point(26, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Rock";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label2.ForeColor = System.Drawing.Color.MistyRose;
+            this.label2.Location = new System.Drawing.Point(26, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Elements of Rock";
+            // 
+            // NumberTxt
+            // 
+            this.NumberTxt.Location = new System.Drawing.Point(216, 34);
+            this.NumberTxt.Name = "NumberTxt";
+            this.NumberTxt.Size = new System.Drawing.Size(191, 20);
+            this.NumberTxt.TabIndex = 6;
+            this.NumberTxt.TextChanged += new System.EventHandler(this.NumberTxt_TextChanged);
+            // 
+            // LetterTxt
+            // 
+            this.LetterTxt.Location = new System.Drawing.Point(216, 75);
+            this.LetterTxt.Name = "LetterTxt";
+            this.LetterTxt.Size = new System.Drawing.Size(191, 20);
+            this.LetterTxt.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.PeachPuff;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(531, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Result";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label3.ForeColor = System.Drawing.Color.MistyRose;
+            this.label3.Location = new System.Drawing.Point(50, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Element a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label4.ForeColor = System.Drawing.Color.MistyRose;
+            this.label4.Location = new System.Drawing.Point(50, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Element b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World);
+            this.label5.ForeColor = System.Drawing.Color.MistyRose;
+            this.label5.Location = new System.Drawing.Point(50, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "a and b";
+            // 
+            // ATxt
+            // 
+            this.ATxt.BackColor = System.Drawing.Color.Gray;
+            this.ATxt.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ATxt.ForeColor = System.Drawing.Color.MistyRose;
+            this.ATxt.Location = new System.Drawing.Point(176, 148);
+            this.ATxt.Multiline = true;
+            this.ATxt.Name = "ATxt";
+            this.ATxt.ReadOnly = true;
+            this.ATxt.Size = new System.Drawing.Size(117, 31);
+            this.ATxt.TabIndex = 8;
+            this.ATxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ATxt.TextChanged += new System.EventHandler(this.ATxt_TextChanged);
+            // 
+            // ABTxt
+            // 
+            this.ABTxt.BackColor = System.Drawing.Color.Gray;
+            this.ABTxt.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ABTxt.ForeColor = System.Drawing.Color.MistyRose;
+            this.ABTxt.Location = new System.Drawing.Point(176, 274);
+            this.ABTxt.Multiline = true;
+            this.ABTxt.Name = "ABTxt";
+            this.ABTxt.ReadOnly = true;
+            this.ABTxt.Size = new System.Drawing.Size(117, 31);
+            this.ABTxt.TabIndex = 10;
+            this.ABTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BTxt
+            // 
+            this.BTxt.BackColor = System.Drawing.Color.Gray;
+            this.BTxt.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTxt.ForeColor = System.Drawing.Color.MistyRose;
+            this.BTxt.Location = new System.Drawing.Point(176, 207);
+            this.BTxt.Multiline = true;
+            this.BTxt.Name = "BTxt";
+            this.BTxt.ReadOnly = true;
+            this.BTxt.Size = new System.Drawing.Size(117, 31);
+            this.BTxt.TabIndex = 9;
+            this.BTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label6.ForeColor = System.Drawing.Color.MistyRose;
+            this.label6.Location = new System.Drawing.Point(453, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Elements Text";
+            // 
+            // LetterLTxt
+            // 
+            this.LetterLTxt.BackColor = System.Drawing.Color.Gray;
+            this.LetterLTxt.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetterLTxt.ForeColor = System.Drawing.Color.MistyRose;
+            this.LetterLTxt.Location = new System.Drawing.Point(445, 42);
+            this.LetterLTxt.Multiline = true;
+            this.LetterLTxt.Name = "LetterLTxt";
+            this.LetterLTxt.ReadOnly = true;
+            this.LetterLTxt.Size = new System.Drawing.Size(158, 84);
+            this.LetterLTxt.TabIndex = 11;
+            // 
+            // ATtxt
+            // 
+            this.ATtxt.BackColor = System.Drawing.Color.Gray;
+            this.ATtxt.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ATtxt.ForeColor = System.Drawing.Color.MistyRose;
+            this.ATtxt.Location = new System.Drawing.Point(458, 144);
+            this.ATtxt.Multiline = true;
+            this.ATtxt.Name = "ATtxt";
+            this.ATtxt.ReadOnly = true;
+            this.ATtxt.Size = new System.Drawing.Size(110, 31);
+            this.ATtxt.TabIndex = 13;
+            this.ATtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label7.ForeColor = System.Drawing.Color.MistyRose;
+            this.label7.Location = new System.Drawing.Point(374, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 30);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Total a";
+            // 
+            // BTtxt
+            // 
+            this.BTtxt.BackColor = System.Drawing.Color.Gray;
+            this.BTtxt.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTtxt.ForeColor = System.Drawing.Color.MistyRose;
+            this.BTtxt.Location = new System.Drawing.Point(458, 207);
+            this.BTtxt.Multiline = true;
+            this.BTtxt.Name = "BTtxt";
+            this.BTtxt.ReadOnly = true;
+            this.BTtxt.Size = new System.Drawing.Size(110, 31);
+            this.BTtxt.TabIndex = 15;
+            this.BTtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)), true);
+            this.label8.ForeColor = System.Drawing.Color.MistyRose;
+            this.label8.Location = new System.Drawing.Point(372, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 30);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Total b";
+            // 
+            // ABTtext
+            // 
+            this.ABTtext.BackColor = System.Drawing.Color.Gray;
+            this.ABTtext.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ABTtext.ForeColor = System.Drawing.Color.MistyRose;
+            this.ABTtext.Location = new System.Drawing.Point(458, 274);
+            this.ABTtext.Multiline = true;
+            this.ABTtext.Name = "ABTtext";
+            this.ABTtext.ReadOnly = true;
+            this.ABTtext.Size = new System.Drawing.Size(110, 31);
+            this.ABTtext.TabIndex = 18;
+            this.ABTtext.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World);
+            this.label9.ForeColor = System.Drawing.Color.MistyRose;
+            this.label9.Location = new System.Drawing.Point(299, 274);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(157, 30);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Total Elements";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(654, 401);
+            this.Controls.Add(this.ABTtext);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.BTtxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ATtxt);
+            this.Controls.Add(this.LetterLTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BTxt);
+            this.Controls.Add(this.ABTxt);
+            this.Controls.Add(this.ATxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LetterTxt);
+            this.Controls.Add(this.NumberTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Rocks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NumberTxt;
+        private System.Windows.Forms.TextBox LetterTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ATxt;
+        private System.Windows.Forms.TextBox ABTxt;
+        private System.Windows.Forms.TextBox BTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox LetterLTxt;
+        private System.Windows.Forms.TextBox ATtxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox BTtxt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox ABTtext;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Rocks/Rocks/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rocks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //-- Summry--/
+            //-- declare 2 char variable so i can search for the A and B Elements of gem rocks --//
+            //-- one int variable to store the user Number form UI  --//
+            //-- one char Array to containt the the User String Elements Letters from UI --//  
+            //-- make any letters enter by user Lower case If it Upper case and deal with The Final Result at  "LetterLTxt" --//
+            int RocksNumber ;
+            char ElementA = 'a';
+            char ElementB = 'b';
+            LetterLTxt.Text = LetterTxt.Text.ToLower();
+            char[] charLetter;
+            //----------------------------------------------------------------//
+            //-- use of if Condition to check for Our Number and Letter --//
+            //-- the number must be 1=<Number<=100 and the Letters L=<6 --//
+            //- our char Array will be equel to Letter string 
+            //-- pass the result to Text Box To Find and Count The ElementA and ElementB --//
+            if (string.IsNullOrEmpty(NumberTxt.Text) || string.IsNullOrEmpty(LetterTxt.Text))
+            {
+                MessageBox.Show("Enter Rock Number and Elements", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);         
+              
+            }
+            else if (!NumberTxt.Text.All(char.IsDigit) || !LetterTxt.Text.All(char.IsLetter))
+            {
+                MessageBox.Show("Enter Rock Number and Elements", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                    RocksNumber = Convert.ToInt16(NumberTxt.Text); 
+                    charLetter = LetterLTxt.Text.ToCharArray();
+                    if (RocksNumber >=1 && RocksNumber<=100 && charLetter.Count()<=6)
+                    {
+                        if (charLetter.Contains(ElementA) && charLetter.Contains(ElementB))
+                        {
+                            MessageBox.Show("this is Gem Rock with Two Element", "Rocks", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            ATxt.Text = "1";
+                            BTxt.Text = "1";
+                            ABTxt.Text = "2";
+                            ATtxt.Text = charLetter.Count(txt => txt == ElementA).ToString();
+                            BTtxt.Text = charLetter.Count(txt => txt == ElementB).ToString();
+                            ABTtext.Text = (Convert.ToInt16(ATtxt.Text) + Convert.ToInt16(BTtxt.Text)).ToString();
+                        }
+                        else if (charLetter.Contains(ElementA) || charLetter.Contains(ElementB))
+                        {
+                            MessageBox.Show("this is Gem Rock with One Element", "Rocks", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            ATxt.Text = Convert.ToInt32( charLetter.Contains(ElementA)).ToString();
+                            BTxt.Text = Convert.ToInt32(charLetter.Contains(ElementB)).ToString();
+                            ABTxt.Text = "1";
+                            ATtxt.Text = charLetter.Count(txt => txt == ElementA).ToString();
+                            BTtxt.Text = charLetter.Count(txt => txt == ElementB).ToString();
+                            ABTtext.Text = (Convert.ToInt32(ATtxt.Text) + Convert.ToInt32(BTtxt.Text)).ToString();
+                        }
+                       
+                        else
+                        {
+                            MessageBox.Show("this is Normal Rock", "Rocks", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            ATxt.Text = "0";
+                            BTxt.Text = "0";
+                            ABTxt.Text = "0";
+                            ATtxt.Text="0";
+                            BTtxt.Text="0";
+                            ABTtext.Text = "0";
+                            
+                        }
+                     
+                    }
+                    else
+                    {
+                        MessageBox.Show("the Rock Number Or Elements not right , enter rock number between 1 and 100 , so element between 1 to 6 letters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+            }
+
+        }
+
+        private void ATxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+        
+
+       
+    }
+}
